12fe4697e471c0ad555e8a4a8a5b8af3b2e79d76
ChmielewskiGianpiero <chmielewskigianpiero@gmail.com>
2023-10-22 23:37:31 +0200

    FIX -  milestone 2

--- js/app.js
+++ js/app.js
@@ -2,93 +2,88 @@
 
 const images = [
 	{
-		image: 'img/01.webp',
-		title: "Marvel's Spiderman Miles Morale",
+		image: './img/01.webp',
+		title: "Marvel's Spiderman Miles Morales",
 		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
 	},
 	{
-		image: 'img/02.webp',
+		image: './img/02.webp',
 		title: 'Ratchet & Clank: Rift Apart',
 		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
 	},
 	{
-		image: 'img/03.webp',
+		image: './img/03.webp',
 		title: 'Fortnite',
 		text: 'Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.',
 	},
 	{
-		image: 'img/04.webp',
+		image: './img/04.webp',
 		title: 'Stray',
 		text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
 	},
 	{
-		image: 'img/05.webp',
+		image: './img/05.webp',
 		title: "Marvel's Avengers",
 		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
 	},
 ]
 
+let currentIndex= 0;
+
 const carouselDOMElement = document.querySelector(".carousel")
 
-for (let i = 0; i < images.length; i++) {
-  const currentImage = images[i];
-  image = currentImage.image;
-  title = currentImage.title;
-  text = currentImage.text;
+for (let i = 0; i < images.length ; i++){
+	const slide = images[i]
+	const image = slide.image;
+	const title = slide.title;
+	const text = slide.text;
 
-  const html = `
-    <img class="carousel-item" src="${ image }" alt="">
-    <h2 class="title-slide">${ title }</h2>
-    <p class="text-slide">${ text }</p>
-  `
-  carouselDOMElement.innerHTML += html;
-  console.log(html);
+	html = `
+	<div class="slide">
+        <img class="img-slide" src="${image}" alt="">
+        <div class="content-slide">
+            <h2 class="title-slide">${title}</h2>
+            <p class="text-slide">${text}</p>            
+		</div>
+    </div>
+	`
+	carouselDOMElement.innerHTML += html;
+	console.log(carouselDOMElement)
 }
 
-  
-const imgDOMElements = document.querySelectorAll(".carousel-item");
-const titleDOMElements = document.querySelectorAll(".title-slide")
-const textDOMElements = document.querySelectorAll(".text-slide");
 
-let currentIndex = 0;
+let slideDOMElement = document.querySelectorAll(".slide")
+// let currentSlide = slideDOMElement[currentIndex]
 
-let currentImg = imgDOMElements[currentIndex];
-let currentTitle = titleDOMElements [currentIndex];
-let currentText = textDOMElements [currentIndex];
-currentImg.classList.add("active");
-currentTitle.classList.add("active");
-currentText.classList.add("active");
+slideDOMElement[currentIndex].classList.add("active")
 
-const btnTop = document.querySelector(".btn-up");
-const btnBottom = document.querySelector(".btn-down");
+const btnTop = document.querySelector(".btn-top")
+const btnDown = document.querySelector(".btn-down")
 
-// btnTop.addEventListener("click,", function(){
-// 	 const nextSlideElement = imgDOMElements[currentIndex + 1]
+btnTop.addEventListener("click", prevSlide );
+btnDown.addEventListener("click" , nextSlide);
 
-// 	 if (nextSlideElement) {
-// 	 	const activeSlideElement = imgDOMElements[currentIndex]
-// 	 	activeSlideElement.classList.remove('active')
-// 	 	nextSlideElement.classList.add('active')
-// 	 	currentIndex++
-// 	 }
-// })
 
- btnTop.addEventListener("click", function () {
-     imgDOMElements[currentIndex].classList.remove("active");
-	 titleDOMElements [currentIndex].classList.remove("active")
-	 textDOMElements [currentIndex].classList.remove("active")
-     currentIndex++;
-     imgDOMElements[currentIndex].classList.add("active");
-	 titleDOMElements [currentIndex].classList.add("active");
-	 textDOMElements [currentIndex].classList.add("active");
- })
+function nextSlide() {
+	slideDOMElement[currentIndex].classList.remove("active")
 
- btnBottom.addEventListener("click", function () {
-	imgDOMElements[currentIndex].classList.remove("active");
-	titleDOMElements [currentIndex].classList.remove("active")
-	textDOMElements [currentIndex].classList.remove("active")
-     currentIndex--;
-     imgDOMElements[currentIndex].classList.add("active");
-	 titleDOMElements [currentIndex].classList.add("active");
-	 textDOMElements [currentIndex].classList.add("active");
- })
+	if (currentIndex === slideDOMElement.length - 1) {
+		currentIndex = 0;
+	} else {
+		currentIndex++;
+	}
+	slideDOMElement[currentIndex].classList.add("active");
+}
+
+function prevSlide(){
+	slideDOMElement[currentIndex].classList.remove("active")
+
+	if (currentIndex === 0) {
+		currentIndex = slideDOMElement.length - 1;
+	} else {
+		currentIndex--;
+	}
+	slideDOMElement[currentIndex].classList.add("active");
+}
+
+setInterval(nextSlide, 3000)
